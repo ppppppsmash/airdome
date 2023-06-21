790d636abf9fe33bd85bae747682f45f5196abd2
ppppppsmash <pppppp.work@gmail.com>
2023-06-21 21:00:36 +0900

    add: テンプレ

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const config = require("./src/config/config.json");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: config.base_path !== "/" ? config.base_path : "",
+  trailingSlash: config.site.trailing_slash,
+};
+
+module.exports = nextConfig;
