4ebd4e636934ee7bdffc24302e2ec8fba7f9dd02
ppppppsmash <pppppp.work@gmail.com>
2023-06-26 23:58:00 +0900

    mod: profileページ

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,13 +2,18 @@ import ImageFallback from "@/components/ImageFallback";
 import MDXContent from "@/components/MDXContent";
 import { getListPage } from "@/lib/contentParser";
 import { markdownify } from "@/lib/utils/textConverter";
+import CallToAction from "@/partials/CallToAction";
 import SeoMeta from "@/partials/SeoMeta";
+import Testimonials from "@/partials/Testimonials";
 import { RegularPage } from "@/types";
+
 
 const Profile = () => {
   const data: RegularPage = getListPage("pages/profile.md");
   const { frontmatter, content } = data;
   const { title, meta_title, description, image } = frontmatter;
+  const testimonial = getListPage("sections/testimonial.md");
+  const callToAction = getListPage("sections/call-to-action.md");
 
   return (
     <>
@@ -18,22 +23,17 @@ const Profile = () => {
         description={description}
         image={image}
       />
+
+      {/* <Testimonials data={testimonial} /> */}
+      {/* <CallToAction data={callToAction} /> */}
+
       <section className="section-sm">
         <div className="container">
           <div className="row justify-center">
             <div className="text-left md:col-10 lg:col-7">
-              {image && (
-                <ImageFallback
-                  className="mx-auto mb-6 rounded-lg"
-                  src={image}
-                  width={1200}
-                  height={200}
-                  alt={title}
-                />
-              )}
               <h2
                 dangerouslySetInnerHTML={markdownify(title)}
-                className="h3 mb-6"
+                className="h3 mb-6 text-center"
               />
               <div className="content">
                 <MDXContent content={content} />
@@ -42,6 +42,7 @@ const Profile = () => {
           </div>
         </div>
       </section>
+
     </>
   );
 };
