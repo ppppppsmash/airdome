50d11240a1df331201ee0f47f6cfff25b35086cb
ppppppsmash <pppppp.work@gmail.com>
2023-06-29 01:19:20 +0900

    mod: sp時NAV動作

--- src/layouts/components/DelaySection.tsx
+++ src/layouts/components/DelaySection.tsx
@@ -10,7 +10,7 @@ const DelaySection = ({
 }) => {
   return (
     <motion.div
-      initial={{ y: 10, opacity: 0 }}
+      initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.4, delay }}
     >
