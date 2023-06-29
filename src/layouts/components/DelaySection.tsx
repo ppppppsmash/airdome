c4886d5b758033a5538440cc68c4d6421d5d9512
ppppppsmash <pppppp.work@gmail.com>
2023-06-29 20:36:15 +0900

    mod: framer-motionをコメントアウト

--- src/layouts/components/DelaySection.tsx
+++ src/layouts/components/DelaySection.tsx
@@ -1,21 +1,27 @@
 "use client";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 
-const DelaySection = ({
-  children,
-  delay = 0
-} : {
-  children: React.ReactNode
-  delay: number
-}) => {
+// const DelaySection = ({
+//   children,
+//   delay = 0
+// } : {
+//   children: React.ReactNode
+//   delay: number
+// }) => {
+//   return (
+//     <motion.div
+//       initial={{ y: 20, opacity: 0 }}
+//       animate={{ y: 0, opacity: 1 }}
+//       transition={{ duration: 0.4, delay }}
+//     >
+//       {children}
+//     </motion.div>
+//   )
+// }
+
+const DelaySection = () => {
   return (
-    <motion.div
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.4, delay }}
-    >
-      {children}
-    </motion.div>
+    <p>テスト</p>
   )
 }
 
