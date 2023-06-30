4d5062c16cb48af32074d618dca848cf02cbc88c
ppppppsmash <pppppp.work@gmail.com>
2023-07-01 00:48:06 +0900

    fix: トップページ修復

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-"use client"
-import dynamic from 'next/dynamic'
 import ImageFallback from "@/components/ImageFallback";
-import { ModelViewer} from "@/components/ModelViewer";
+import { ModelViewer } from "@/components/ModelViewer";
 import { getListPage } from "@/lib/contentParser";
 import { markdownify } from "@/lib/utils/textConverter";
 import CallToAction from "@/partials/CallToAction";
@@ -9,32 +7,105 @@ import SeoMeta from "@/partials/SeoMeta";
 import Testimonials from "@/partials/Testimonials";
 import { Button, Feature } from "@/types";
 import { FaCheck } from "react-icons/fa/index.js";
-import { Canvas } from '@react-three/fiber';
-//const Duck = dynamic(() => import('@/components/ModelViewer').then((mod) => mod.Duck), { ssr: false })
-const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
 const Home = () => {
-  // const homepage = getListPage("_index.md");
-  // const testimonial = getListPage("sections/testimonial.md");
-  // const callToAction = getListPage("sections/call-to-action.md");
-  // const { frontmatter } = homepage;
-  // const {
-  //   banner,
-  //   features,
-  // }: {
-  //   banner: { title: string; image: string; content?: string; button?: Button };
-  //   features: Feature[];
-  // } = frontmatter;
+  const homepage = getListPage("_index.md");
+  const testimonial = getListPage("sections/testimonial.md");
+  const callToAction = getListPage("sections/call-to-action.md");
+  const { frontmatter } = homepage;
+  const {
+    banner,
+    features,
+  }: {
+    banner: { title: string; image: string; content?: string; button?: Button };
+    features: Feature[];
+  } = frontmatter;
 
   return (
     <>
+      <SeoMeta />
       <section className="sm:section pt-14">
         <div className="container">
           <div className="row justify-center">
-            <ModelViewer/>
+            <div className="sm:mb-16 text-center lg:col-7">
+              <h1
+                className="mb-4"
+                dangerouslySetInnerHTML={markdownify(banner.title)}
+              />
+              <p
+                dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
+              />
+              {banner.button!.enable && (
+                <a className="btn btn-primary" href={banner.button!.link}>
+                  {banner.button!.label}
+                </a>
+              )}
+            </div>
+
+            <ModelViewer />
           </div>
         </div>
       </section>
+
+      {features.map((feature, index: number) => (
+        <section
+          key={index}
+          className={`section-sm ${index % 2 === 0 && "bg-gradient"}`}
+        >
+          <div className="container">
+            <div className="row items-center justify-between">
+              <div
+                className={`mb:md-0 mb-6 md:col-5 ${
+                  index % 2 !== 0 && "md:order-2"
+                }`}
+              >
+                <ImageFallback
+                  src={feature.image}
+                  height={480}
+                  width={520}
+                  alt={feature.title}
+                />
+              </div>
+              <div
+                className={`md:col-7 lg:col-6 ${
+                  index % 2 !== 0 && "md:order-1"
+                }`}
+              >
+                <h2
+                  className="mb-4"
+                  dangerouslySetInnerHTML={markdownify(feature.title)}
+                />
+                <p
+                  className="mb-8 text-lg"
+                  dangerouslySetInnerHTML={markdownify(feature.content)}
+                />
+                {feature.sub_content &&
+                <p
+                  className="mb-2 text-lg"
+                  dangerouslySetInnerHTML={markdownify(feature.sub_content)}
+                />
+                }
+                <ul>
+                  {feature.bulletpoints.map((bullet: string) => (
+                    <li className="relative mb-4 pl-6" key={bullet}>
+                      <FaCheck className={"absolute left-0 top-1.5"} />
+                      <span dangerouslySetInnerHTML={markdownify(bullet)} />
+                    </li>
+                  ))}
+                </ul>
+                {feature.button.enable && (
+                  <a
+                    className="btn btn-primary mt-5"
+                    href={feature.button.link}
+                  >
+                    {feature.button.label}
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+      ))}
     </>
   );
 };
--- src/layouts/components/ModelViewer.tsx
+++ src/layouts/components/ModelViewer.tsx
@@ -1,48 +1,4 @@
-// "use client"
-// import { useMediaQuery } from "react-responsive"
-// import { Canvas } from "@react-three/fiber";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader"
-// import { useLoader } from "@react-three/fiber";
-// import { Stage, OrbitControls } from "@react-three/drei";
-// import { useGLTF } from '@react-three/drei'
-
-// const Airdome = () => {
-//   const model = useLoader(ColladaLoader, "/models/model.dae");
-//   return (
-//     <>
-//       <ambientLight />
-//       <Stage intensity={0.5} environment="city" shadows={{ type: 'accumulative', bias: -0.001 }} adjustCamera={true}>
-//         <group>
-//           <primitive
-//             object={model.scene}
-//           />
-//         </group>
-//       </Stage>
-//       {/* <OrbitControls autoRotate autoRotateSpeed={0.5} enableZoom={true} makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} minDistance={50}  // カメラとモデルの最小距離を40に設定
-//           maxDistance={80} /> */}
-//       <OrbitControls autoRotate autoRotateSpeed={0.5} enableZoom={true} makeDefault minDistance={50}  // カメラとモデルの最小距離を40に設定
-//       maxDistance={80} />
-//     </>
-//   );
-// };
-
-// const ModelViewer = () => {
-  // const isMobile = useMediaQuery({ maxWidth: 767 });
-  // const zoom = isMobile ? 1 : 3;
-//   return (
-    // <Canvas
-    //   className="w-full !md:h-[800px] !h-[350px]"
-    //   shadows camera={{ position: [-80, 10, 50], fov: 15, zoom }}
-    // >
-//       <Airdome />
-//     </Canvas>
-//   );
-// };
-
-// export {ModelViewer};
-
-"use client"
+"use client";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Stage, OrbitControls } from "@react-three/drei";
 import { useGLTF } from '@react-three/drei';
@@ -77,7 +33,7 @@ export function ModelViewer() {
   return (
     <Canvas
       className="w-full !md:h-[800px] !h-[350px]"
-      shadows camera={{ position: [-80, 10, 50], fov: 15 }}
+      shadows camera={{ position: [-80, 10, 50], fov: 15, zoom }}
     >
       <Airdome/>
     </Canvas>
