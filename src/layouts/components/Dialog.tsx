39610c5ef1ccac337f5f0fb3253fb262f17baf09
ppppppsmash <pppppp.work@gmail.com>
2023-08-06 23:03:29 +0900

    mod: pdfボタン仕様&ファイル

--- src/layouts/components/Dialog.tsx
+++ src/layouts/components/Dialog.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
-import { BsFiletypePdf } from 'react-icons/bs/'
+import { BsFiletypePdf } from 'react-icons/bs'
+import { BiDownload } from 'react-icons/bi'
+import Link from 'next/link';
 
 const DialogParts = () => {
   return (
@@ -11,45 +13,24 @@ const DialogParts = () => {
         bottom-24 right-4 w-[60px] h-[60px] hover:scale-[0.9] transition
         text-black shadow-blackA7 hover:bg-mauve3 inline-flex items-center
         justify-center bg-white px-[15px] leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none rounded-full">
-      <BsFiletypePdf size={25} /><span className="lg:block lg:text-[26px] pl-2 font-thin hidden">气膜资料请求</span>
+      <BsFiletypePdf size={28} /><span className="lg:block lg:text-[22px] pl-2 font-thin hidden">气膜资料请求</span>
       </button>
     </Dialog.Trigger>
     <Dialog.Portal>
       <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
       <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-        <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-          气膜资料请求
+        <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium mb-8">
+          下载/浏览 气膜PDF文件
         </Dialog.Title>
-        <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-          测试测试
+        <Dialog.Description className="text-mauve11 mt-[10px] ml-2 mb-5 text-[15px] leading-normal">
+          <p className="mb-4 flex gap-x-2">
+          <BiDownload size={20} /><Link className="mb-4 border-black border-b-2 border-dotted" target="_blank" href="/file/Broadwell_Logistic_and_Warehouse_Dome.pdf">博德维物流与气膜说明.pdf</Link>
+          </p>
+          <p className="flex gap-x-2">
+          <BiDownload size={20} /><Link className="border-black border-b-2 border-dotted" target="_blank" href="/file/Broadwell_Air_Dome_General_Instruction.pdf">博德维气膜一般说明.pdf</Link>
+          </p>
         </Dialog.Description>
-        <fieldset className="mb-[15px] flex items-center gap-5">
-          <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
-            id="name"
-            defaultValue="Pedro Duarte"
-          />
-        </fieldset>
-        <fieldset className="mb-[15px] flex items-center gap-5">
-          <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="username">
-            Username
-          </label>
-          <input
-            className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
-            id="username"
-            defaultValue="@peduarte"
-          />
-        </fieldset>
-        <div className="mt-[25px] flex justify-end">
-          <Dialog.Close asChild>
-            <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
-              Save changes
-            </button>
-          </Dialog.Close>
-        </div>
+
         <Dialog.Close asChild>
           <button
             className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
