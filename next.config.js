f975c0f15f6f4d4deb873b9098609433cd7ebcae
ppppppsmash <pppppp.work@gmail.com>
2024-07-10 00:33:27 +0900

    fix: landing page dir setting

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const config = require("./src/config/config.json");
-const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 
@@ -7,7 +6,6 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
-  i18n
 };
 
 module.exports = nextConfig;
